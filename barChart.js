2cfc60d89d2baa367927fbfd49f3e20bc81580c0
Jiayu Jiang <you@example.com>
2019-03-12 15:00:13 -0400

    h

--- barChart.js
+++ barChart.js
@@ -5,16 +5,22 @@ var drawGraph = function(data) //data is an object
   var svg = d3.select
 }
 
-
 var updateDay = function(dayChange)
 {
-  var day = document.getElementById("dayNumber").textContent;
-  day = day + dayChange;
+  var day = Number(document.getElementById("dayNumber").textContent); //get from the paragrph
+  day = day + Number(dayChange);
+  console.log(day);
 
   dataP.then(function(data)
   {
     updateGraph(data[day - 1])
-  })
+  },
+  function(err)
+  {
+    console.log(err);
+  });
+
+  document.getElementById("dayNumber").innerHTML = day; //update the paragrph
 }
 
 dataP.then(function(data)
