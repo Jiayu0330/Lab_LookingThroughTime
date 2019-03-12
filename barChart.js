2b07da70d67ecde6e2c99b70fbac347150b3bbbd
Jiayu Jiang <you@example.com>
2019-03-12 14:53:58 -0400

    h

--- barChart.js
+++ barChart.js
@@ -2,7 +2,19 @@ var dataP = d3.json("gradeDataTime.json");
 
 var drawGraph = function(data) //data is an object
 {
-  
+  var svg = d3.select
+}
+
+
+var updateDay = function(dayChange)
+{
+  var day = document.getElementById("dayNumber").textContent;
+  day = day + dayChange;
+
+  dataP.then(function(data)
+  {
+    updateGraph(data[day - 1])
+  })
 }
 
 dataP.then(function(data)
