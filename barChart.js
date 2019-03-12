dee45d287a3201ed6b64709b3413f0cc4c20f2e4
Jiayu Jiang <you@example.com>
2019-03-12 15:35:26 -0400

    h

--- barChart.js
+++ barChart.js
@@ -2,18 +2,53 @@ var dataP = d3.json("gradeDataTime.json");
 
 var drawGraph = function(data) //data is an object
 {
-  var svg = d3.select
+  var width = 400;
+  var height = 300;
+  var barWidth = width/data.grades.length;
+
+  var svg = d3.select("svg")
+              .attr("width", width)
+              .attr("height", height)
+
+  svg.selectAll("rect")
+     .data(data.grades)
+     .enter()
+     .append("rect")
+     .attr("x", function(d,i) {return i * barWidth;} )
+     .attr("y", function(d) {return height - d.grade*3;} )
+     .attr("width", barWidth)
+     .attr("height", function(d) {return d.grade*3;} )
+     .attr("fill", "black")
+     .attr("stroke", "white")
+     .attr("stroke-width", 2);
 }
+
+var updateGraph = function(data)
+{
+  var width = 400;
+  var height = 300;
+  var barWidth = width/data.grades.length
+
+  d3.select("svg")
+    .selectAll("rec")
+    .data(data.grades)
+    .attr("x", function(d,i) {return i * barWidth;} )
+    .attr("y", function(d) {return height - d.grade*3;} )
+    .attr("width", barWidth)
+    .attr("height", function(d) {return d.grade*3;} );
+
+}
+
 
 var updateDay = function(dayChange)
 {
   var day = Number(document.getElementById("dayNumber").textContent); //get from the paragrph
   day = day + Number(dayChange);
-  console.log(day);
 
   dataP.then(function(data)
   {
     updateGraph(data[day - 1])
+    console.log(day - 1);
   },
   function(err)
   {
