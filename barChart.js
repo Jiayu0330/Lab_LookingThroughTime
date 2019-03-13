449df19c5839a4d0ff6ec73bf2cebb03e6b4e839
Jiayu Jiang <you@example.com>
2019-03-12 21:24:28 -0400

    add legend and y-axis

--- barChart.js
+++ barChart.js
@@ -2,43 +2,117 @@ var dataP = d3.json("gradeDataTime.json");
 
 var drawGraph = function(data) //data is an object
 {
-  var width = 400;
-  var height = 300;
-  var barWidth = width/data.grades.length;
+  var screen = {
+    width: 350,
+    height: 320 //always change height in updateGraph function as well
+  }
+
+  var margins = {
+    left: 55, //space for y-axis
+    top: 10, //space for stroke
+    bottom: 40, //space for stroke
+    right :0 //space for legend
+  }
+
+  var width = screen.width - margins.left - margins.right;
+  var height = screen.height - margins.top - margins.bottom;
+
+  var barWidth = width/data.length;
+
+  var innerPadding = 20;
+
+  var xScale = d3.scaleLinear()
+                 .domain([0, data.length]) //first test, second test, thrid...
+                 .range([margins.left, width]);
+
+  var yScale = d3.scaleLinear()
+                 .domain([0, 100])
+                 .range([height, 0]);
+
+  var colors = d3.scaleOrdinal(d3.schemeSet3);
 
   var svg = d3.select("svg")
-              .attr("width", width)
-              .attr("height", height)
+              .attr("width", screen.width)
+              .attr("height", screen.height)
 
   svg.selectAll("rect")
-     .data(data.grades)
+     .data(data)
      .enter()
      .append("rect")
-     .attr("x", function(d,i) {return i * barWidth;} )
-     .attr("y", function(d) {return height - d.grade*3;} )
-     .attr("width", barWidth)
-     .attr("height", function(d) {return d.grade*3;} )
-     .attr("fill", "black")
-     .attr("stroke", "white")
+     .attr("x", function(d,i) {return i * barWidth + margins.left;} )
+     .attr("y", function(d) {return yScale(d.grade) + margins.top;} )
+     .attr("width", barWidth - innerPadding)
+     .attr("height", function(d) {return height - yScale(d.grade);} )
+     .attr("fill", function(d) {return colors(d.name);} )
+     .attr("stroke", "#563866")
      .attr("stroke-width", 2);
+
+  //y-axis
+  var yAxis = d3.axisLeft(yScale);
+
+  svg.append("g")
+     .classed("yAxis", true)
+     .call(yAxis)
+     .attr("transform", "translate("+ (margins.left - innerPadding) +", "+ margins.top +")")
+     .attr("color", "#563866")
+     .style("font-size", "15")
+     .style("font-weight", "bold");
+
+  //legend
+  var legend = svg.append("g")
+                  .classed("legend",true)
+                  .attr("transform","translate(8, "+ (height + 5) +")")
+
+  var legendLines = legend.selectAll("g")
+                          .data(data)
+                          .enter()
+                          .append("g")
+                          .classed("legendLines",true)
+                          .attr("transform",function(d,i) {return "translate("+ (i * barWidth) +", 0)"});
+
+  /*legendLines.append("circle")
+                .attr("cx",40)
+                .attr("cy",height-250)
+                .attr("r",7)
+                .attr("fill",function(d) {return colors(d.name)})
+                .attr("stroke","#1F3641")
+                .attr("stroke-width",3);*/
+
+  legendLines.append("text")
+                .attr("x",50)
+                .attr("y",height-245)
+                .text(function(d) {return d.name})
+                .attr("fill","#563866")
+                .style("font-size", "18")
+                .style("font-style", "italic")
+                .style("font-weight", "bold");
+
+
 }
 
 var updateGraph = function(data)
 {
-  var width = 400;
-  var height = 300;
-  var barWidth = width/data.grades.length
+  var margins = {
+    left: 50, //space for y-axis
+    top: 10, //space for stroke
+    bottom: 50, //space for stroke
+    right :110 //space for legend
+  }
 
+  var height = 320 - margins.top - margins.bottom
+
+  var yScale = d3.scaleLinear()
+                 .domain([0, 100])
+                 .range([height, margins.top]);
+
+  //only change y-position and height
   d3.select("svg")
-    .selectAll("rec")
-    .data(data.grades)
-    .attr("x", function(d,i) {return i * barWidth;} )
-    .attr("y", function(d) {return height - d.grade*3;} )
-    .attr("width", barWidth)
-    .attr("height", function(d) {return d.grade*3;} );
+    .selectAll("rect")
+    .data(data)
+    .attr("y", function(d) {return yScale(d.grade);} )
+    .attr("height", function(d) {return height - yScale(d.grade) + margins.top;} );
 
 }
-
 
 var updateDay = function(dayChange)
 {
@@ -47,7 +121,7 @@ var updateDay = function(dayChange)
 
   dataP.then(function(data)
   {
-    updateGraph(data[day - 1])
+    updateGraph(data[day - 1].grades)
     console.log(day - 1);
   },
   function(err)
@@ -60,7 +134,7 @@ var updateDay = function(dayChange)
 
 dataP.then(function(data)
 {
-  drawGraph(data[0]);
+  drawGraph(data[0].grades);
 },
 function(err)
 {
